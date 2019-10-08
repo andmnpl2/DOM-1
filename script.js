049cdf4da9d5d42beb0731056b81d8a48687e23c
andmnpl2 <52722017+andmnpl2@users.noreply.github.com>
2019-10-07 19:21:30 -0500

    to web

--- script.js
+++ script.js
@@ -56,7 +56,21 @@
         //append the <h1> and <p> to it before adding it to the container.
 
     
-    let btn = document.querySelector('#btn');
-    btn.addEventListener('click', () => {
-        alert("Hello World");
-      });
+        btn.addEventListener('click', function(e){
+            console.log(e.target);
+          })
+          btn.addEventListener('click', function(e){
+            e.target.style.background = 'blue';
+          })
+
+          // buttons is a node list. It looks and acts much like an array.
+            const buttons = document.querySelectorAll('button');
+
+            // we use the .forEach method to iterate through each button
+            buttons.forEach((button) => {
+
+             // and for each one we add a 'click' listener
+             button.addEventListener('click', (e) => {
+              alert(button.id);
+          });
+        });
